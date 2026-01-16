29bfdf4fe2a132afcada657f17fe35437c492a64
fuzze <tomrun72@gmail.com>
2026-01-16 14:38:34 +0100

    v3

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,8 +1,45 @@
+import org.example.Calculator;
 import org.testng.annotations.Test;
 
 public class TestMain {
+
     @Test
-    void test1() {
-        assert true;
+    void test1(){
+        int x = 5;
+        int y = 5;
+        // Teraz sprawdzamy, czy 5 jest różne od 5. Fałsz.
+        assert x != y;
+    }
+
+    @Test
+    void test2(){
+        int x = 7;
+        int y = 5;
+        // Sprawdzamy, czy różnica nie jest zerem. 2 != 0. PRAWDA.
+        assert 0 != x - y;
+    }
+
+    @Test
+    void test3(){
+        int x = 5;
+        int y = 6;
+        // Sprawdzamy, czy 10 nie równa się 6. PRAWDA.
+        assert y != x * 2;
+    }
+
+    @Test
+    void test4(){
+        Calculator cal = new Calculator();
+        int y = cal.power(5);
+        // Sprawdzamy, czy 5^2 to 25. PRAWDA.
+        assert 25 == y;
+    }
+
+    @Test
+    void test5(){
+        Calculator cal = new Calculator();
+        int y = cal.power(6);
+        // Sprawdzamy, czy 6^2 (36) to nie 35. PRAWDA.
+        assert 36 == y;
     }
 }
